a967e62686fedefbd59c937d63ad7a370be48ef5
Medhashis_Maiti <medhashis000@gmail.com>
2024-01-16 14:35:32 +0530

    customs buttons , added picture, ready for final submission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 import SkillForm from '@/components/SkillForm'
+import { Button } from '@/components/ui/Button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-     <p> medhashis maiti </p>
-     <p> medhashis maiti </p>
+    <div className="flex min-h-screen flex-col  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 items-center gap-10 p-24">
+   
+   <div className=' flex flex-col gap-3 items-center  '>
+    <p className='font-bold text-md text-slate-600 opacity-90  '> Zepto assignment Medhashis Maiti </p>
+   <p className='font-bold text-2xl text-slate-50'> 
+    These are the only items that are present.
+   </p>
+    </div>
+   <div className = " flex  md:96 w-80 flex-wrap gap-4">
+   <Button variant={"default"} className=' rounded-full'> Typescript</Button>
+   <Button variant={"default"} className=' rounded-full'> redux</Button>
+   <Button variant={"default"} className=' rounded-full'>JavaScript</Button>
+   <Button variant={"default"} className=' rounded-full'> Penguin</Button>
+   <Button variant={"default"} className=' rounded-full'> Lion</Button>
+   <Button variant={"default"} className=' rounded-full'> giraffe</Button>
+   <Button variant={"default"} className=' rounded-full'> elephant</Button>
+   <Button variant={"default"} className=' rounded-full'> Kangaroo</Button>
+   <Button variant={"default"} className=' rounded-full'> readme</Button>
+   <Button variant={"default"} className=' rounded-full'> react</Button>
+   </div>
+
      <SkillForm/>
     </div>
   )
--- src/components/SkillForm.tsx
+++ src/components/SkillForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
+import { Button } from "./ui/Button";
 
 interface SkillSuggestion {
   id: number;
@@ -14,19 +15,22 @@ const SkillForm: React.FC = () => {
   const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
   const [tags, setTags] = useState<string[]>([]);
 
-   // Simulate API call with dummy suggestions in JSON format
-   const dummySuggestionsData: SkillSuggestion[] = [
+  // Simulate API call with dummy suggestions in JSON format
+  const dummySuggestionsData: SkillSuggestion[] = [
     { id: 1, suggestions: "TypeScript", image: "/MedhaPersonalLogo.png" },
     { id: 2, suggestions: "React", image: "/MedhaPersonalLogo.png" },
     { id: 3, suggestions: "JavaScript", image: "/MedhaPersonalLogo.png" },
     { id: 4, suggestions: "redux", image: "/MedhaPersonalLogo.png" },
     { id: 5, suggestions: "readme", image: "/MedhaPersonalLogo.png" },
+    { id: 6, suggestions: "Lion", image: "/lion.png" },
+    { id: 7, suggestions: "Elephant", image: "/elephant.png" },
+    { id: 8, suggestions: "Giraffe", image: "/giraffe.jpg" },
+    { id: 9, suggestions: "Penguin", image: "/penguin.jpg" },
+    { id: 10, suggestions: "Kangaroo", image: "/kangaroo.jpg" },
   ];
 
   const getSkillSuggestions = async (input: string) => {
     try {
-     
-
       const enteredSkills = input.split(" ");
       const lastEnteredSkill = enteredSkills[enteredSkills.length - 1];
 
@@ -57,7 +61,7 @@ const SkillForm: React.FC = () => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
   };
-
+// click suggesstions
   const handleSuggestionClick = (suggestion: SkillSuggestion) => {
     setInput((prevInput) => {
       const enteredSkills = prevInput.split(" ");
@@ -76,11 +80,12 @@ const SkillForm: React.FC = () => {
     setShowSuggestions(false);
     setInput("");
   };
-
+// cross icon
   const removeTag = (indexToRemove: number) => {
     setTags([...tags.filter((_, index) => index !== indexToRemove)]);
   };
 
+  // tags 
   const addTag = () => {
     if (input !== "" && !input.includes(" ")) {
       setTags([...tags, input]);
@@ -89,6 +94,7 @@ const SkillForm: React.FC = () => {
     }
   };
 
+  // logic to handle conditional backspace
   const handleBackspace = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Backspace" && input === "") {
       // Handle backspace logic here (e.g., remove the last tag)
@@ -99,6 +105,10 @@ const SkillForm: React.FC = () => {
         setTags(newTags);
       }
     }
+  };
+ // for the reset button
+  const resetTags = () => {
+    setTags([]);
   };
 
   useEffect(() => {
@@ -111,83 +121,89 @@ const SkillForm: React.FC = () => {
 
     getSkillSuggestions(input);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [input, tags]); // Include 'tags' in the dependency array to re-run the effect when tags change
+  }, [input, tags]); 
 
   return (
-    <div className="relative">
-      <div className="tags-input">
-        <ul id="tags">
-          {tags.map((tag, index) => {
-
-const suggestion = dummySuggestionsData.find((s) => s.suggestions === tag);
-console.log(suggestion);
-            return (
-              <li
-                key={index}
-                className={` flex  flex-row items-center  gap-3 tag ${
-                  index === tags.length - 1 ? "bg-sky-100" : ""
-                }`}
-              >
-                {/* <Image
+    <div className=" flex flex-col items-center gap-10">
+      <div className="relative">
+        <div className="tags-input">
+          <ul id="tags">
+            {tags.map((tag, index) => {
+              const suggestion = dummySuggestionsData.find(
+                (s) => s.suggestions === tag
+              );
+              // console.log(suggestion);
+              return (
+                <li
+                  key={index}
+                  className={` flex  flex-row items-center  gap-3 tag ${
+                    index === tags.length - 1 ? "bg-sky-200" : ""
+                  }`}
+                >
+                  {/* <Image
                   src={suggestion?.image}
                   width={28}
                   height={28}
                   alt=" image "
                   className=" rounded-full object-contain"
                 /> */}
-                 {suggestion && suggestion.image && (
-        <Image
-          src={suggestion.image}
-          width={28}
-          height={28}
-          alt="image"
-          className="rounded-full object-contain z-10"
-        />
-      )}
-                <span className="tag-title">{tag}</span>
-                <span
-                  className="tag-close-icon"
-                  onClick={() => removeTag(index)}
-                >
-                  x
-                </span>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="relative">
-          <input
-            type="text"
-            value={input}
-            onChange={handleInputChange}
-            onKeyUp={(event) => (event.key === "Enter" ? addTag() : null)}
-            onKeyDown={handleBackspace}
-            placeholder="Type your skills..."
-          />
-          <div className=" absolute inset-5 backdrop-blur-2xlmt-2 ml-5 z-10 w-48 ">
-            {showSuggestions && suggestions.length > 0 && (
-              <ul className="shadow shadow-slate-500  bg-slate-100 rounded-lg">
-                {suggestions.map((suggestion) => (
-                  <li
-                    key={suggestion.id}
-                    className=" flex  flex-row items-center  gap-3 cursor-pointer   hover:bg-gray-200 p-2 "
-                    onClick={() => handleSuggestionClick(suggestion)}
-                  >
+                  {suggestion && suggestion.image && (
                     <Image
                       src={suggestion.image}
-                      width={40}
-                      height={40}
-                      alt=" image "
-                      className=" rounded-full"
+                      width={28}
+                      height={28}
+                      alt="image"
+                      className="rounded-full object-contain z-10"
                     />
-                    <p> {suggestion.suggestions}</p>
-                  </li>
-                ))}
-              </ul>
-            )}
+                  )}
+                  <span className="tag-title">{tag}</span>
+                  <span
+                    className="tag-close-icon"
+                    onClick={() => removeTag(index)}
+                  >
+                    x
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="relative">
+            <input
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              onKeyUp={(event) => (event.key === "Enter" ? addTag() : null)}
+              onKeyDown={handleBackspace}
+              placeholder="Typing......"
+            />
+            <div className=" absolute inset-5 backdrop-blur-2xlmt-2 ml-5 z-10 w-48 ">
+              {showSuggestions && suggestions.length > 0 && (
+                <ul className="shadow shadow-slate-200  bg-slate-100 rounded-lg">
+                  {suggestions.map((suggestion) => (
+                    <li
+                      key={suggestion.id}
+                      className=" flex  flex-row items-center  gap-3 cursor-pointer rounded-lg  hover:bg-gray-200 p-2 "
+                      onClick={() => handleSuggestionClick(suggestion)}
+                    >
+                      <Image
+                        src={suggestion.image}
+                        width={40}
+                        height={40}
+                        alt=" image "
+                        className=" rounded-full"
+                      />
+                      <p> {suggestion.suggestions}</p>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
           </div>
         </div>
       </div>
+      <Button className=" " variant={"destructive"} onClick={resetTags}>
+        Clear all{" "}
+      </Button>
     </div>
   );
 };
